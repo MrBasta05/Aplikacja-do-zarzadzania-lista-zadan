ff56e811006a22fab5163ca305925a2a0ba9db82
TutusBOT <barte77pl@gmail.com>
2024-05-25 22:26:28 +0200

    usuwam elo

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -14,31 +14,3 @@ bool MyApp::OnInit() {
 
     return true;
 }
-
-
-//#include <wx/wx.h>
-//
-//class App : public wxApp {
-//public:
-//    bool OnInit() {
-//        wxFrame* window = new wxFrame(NULL, wxID_ANY, "GUI Test", wxDefaultPosition, wxSize(690, 690));
-//        wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-//        wxPanel* panel = new wxPanel(this, -1);
-//        taskList = new wxListBox(panel, wxID_ANY, wxPoint(10, 10), wxSize(200, 300));
-//        taskList->AppendString("task");
-//
-//        wxStaticText* text = new wxStaticText(window, wxID_ANY, "FIUUUUUUUUUT",
-//            wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
-//        wxStaticText* text2 = new wxStaticText(window, wxID_ANY, "Dziekan skurwysyn robi mi loda",
-//            wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
-//        text->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
-//        sizer->Add(text, 1, wxALIGN_CENTER);
-//        sizer->Add(text2, 1, wxALIGN_CENTER);
-//        window->SetSizer(sizer);
-//        window->Show();
-//        return true;
-//    }
-//};
-//
-////heloooo
-//wxIMPLEMENT_APP(App);
